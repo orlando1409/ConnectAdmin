3ab12bd7f7a885c08dc01b7ef1c53d9d4f41fb53
Estefano Castañeda <mixc21@gmail.com>
2017-01-24 09:53:22 -0500

    creating pieces

--- src/js/client.js
+++ src/js/client.js
@@ -1,42 +1,7 @@
-/*import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>Work</h1>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 
 ReactDOM.render(<Layout />, app);
-*/
-import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
-import ReactDOM from "react-dom";
-
-export default class MenuExampleInvertedSecondary extends Component {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { activeItem } = this.state
-
-    return (
-      <Segment inverted>
-        <Menu inverted pointing secondary>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
-        </Menu>
-      </Segment>
-    )
-  }
-}
-
-const app = document.getElementById('app');
-
-ReactDOM.render(<MenuExampleInvertedSecondary />, app);
---
+++ src/js/components/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header";
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <Header />
+    )
+  }
+}
