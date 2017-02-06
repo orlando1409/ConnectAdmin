52b6e58be6267360ea0bde150da711e31b0b9853
Estefano Castañeda <mixc21@gmail.com>
2017-02-06 15:32:10 -0500

    reviewing redux components and login

--- src/js/pages/Dashboard.js
+++ src/js/pages/Dashboard.js
@@ -3,6 +3,64 @@ import CardList from '../components/CardList'
 import { Button, Icon, Grid, Divider, Segment } from 'semantic-ui-react'
 import PopupBtn from '../components/PopupBtn'
 
+import { applyMiddleware, createStore} from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import axios from "axios";
+import promise from "redux-promise-middleware";
+
+const initialState = {
+  fetching: false,
+  fetched: false,
+  user: [],
+  error: null
+}
+
+const reducer = (state={}, action) => {
+  switch (action.type) {
+    case "FETCH_USER_PENDING": {
+      return {...state, fetching: true}
+      break;
+    }
+    case "FETCH_USER_REJECTED": {
+      return {...state, fetching: false, error: action.payload}
+      break;
+    }
+    case "FETCH_USER_FULFILLED": {
+      return {...state, fetching: false, fetched: true, user: action.payload.data}
+      break;
+    }
+  }
+
+  return state
+}
+
+const middleware = applyMiddleware(promise(), thunk, logger())
+const store = createStore(reducer, middleware)
+
+store.dispatch({
+  type: "FETCH_USER",
+  payload: axios.post("https://bxconnect.herokuapp.com:443/api/employee/authenticate/", {
+    username: "ecastaneda",
+    password: "allstars"
+  })
+});
+
+/*store.dispatch((dispatch) => {
+  dispatch({type: "FETCH_USER"})
+  axios.post("https://bxconnect.herokuapp.com:443/api/employee/authenticate/", {
+    username: "ecastaneda",
+    password: "allstars"
+  }).then((response) => {
+    dispatch({type: "RECEIVE_USER", payload: response})
+  }).catch((error) => {
+    dispatch({type: "FETCH_ERROR", payload: error})
+  })
+//async
+  dispatch({type: "BAR"})
+});*/
+
+
 export default class Dashboard extends React.Component {
   render () {
     return (
